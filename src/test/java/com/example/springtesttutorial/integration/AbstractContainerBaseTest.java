cf994cf18fbb88d54c44d53370d2e1d8fd69bc8a
Stefan Szekely <sekestefan@gmail.com>
2023-06-03 15:28:47 +0200

    add: TestContainers

---
+++ src/test/java/com/example/springtesttutorial/integration/AbstractContainerBaseTest.java
@@ -0,0 +1,29 @@
+package com.example.springtesttutorial.integration;
+
+
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+public abstract class AbstractContainerBaseTest {
+
+  static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
+
+  static {
+    POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:15.3")
+    .withDatabaseName("integration-tests-db")
+    .withUsername("username")
+    .withPassword("password");
+
+    POSTGRE_SQL_CONTAINER.start();
+  }
+
+  @DynamicPropertySource
+  public static void dynamicPropertySource(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+    registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+    registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
+  }
+}
